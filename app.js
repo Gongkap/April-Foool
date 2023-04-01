9155fce63e945e67ff6bef8f0b851ac9094a87d6
gongkap <tamegongkaptarh12@gmail.com>
2023-04-01 11:57:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const port =process.env.port || 3000;
+const express =require('express');
+const ejs=require('ejs');
+const path =require('path');
+
+
+const app=express();
+
+app.set('view engine','ejs');
+app.use('/public',express.static(path.join(__dirname,'public')));
+app.use('/css',express.static(path.join(__dirname,'./node_modules/bootstrap/dist/css')));
+app.use('/js',express.static(path.join(__dirname,'./node_modules/bootstrap/dist/js')))
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.listen(port,()=>{
+    console.log(`listening on the port ${port}`)
+})
